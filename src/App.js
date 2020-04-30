8e21f40c6d3e3b3a657bb57872793f3fb3374426
Daniel Stracquadanio <daniel.stracquadanio@gmail.com>
2020-04-30 17:17:17 -0400

    Able to fetch data from firestore

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import firestore from "./firestore";
+import Todo from "./Todo/Todo";
 
-function App() {
-  const todoList = async () => {
-    let todos = firestore.collection("todos");
-    let todoList = await todos.get();
-    todoList.docs.forEach((doc) => console.log(doc.data()));
-  };
-  todoList();
-  return <div className="App"></div>;
+export default function App() {
+  const [todos, setTodo] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      let data = firestore.collection("todos");
+      let list = await data.get();
+      list.docs.forEach((todo) => setTodo((todos) => [...todos, todo.data()]));
+    }
+    getData();
+  }, []);
+  return (
+    <div>
+      {todos.map((todo) => (
+        <h1>{todo.name}</h1>
+      ))}
+      <Todo />
+    </div>
+  );
 }
-
-export default App;
