78abe520196cc86f0d1282074de437f82faf5591
Daniel Stracquadanio <daniel.stracquadanio@gmail.com>
2020-04-30 21:34:22 -0400

    new items populate without reloading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import firestore from "./firestore";
 import Todo from "./Todo/Todo";
 
 export default function App() {
-  const [todos, setTodo] = useState([]);
-  useEffect(() => {
-    async function getData() {
-      let data = firestore.collection("todos");
-      let list = await data.get();
-      list.docs.forEach((todo) => setTodo((todos) => [...todos, todo.data()]));
-    }
-    getData();
-  }, []);
   return (
     <div>
-      <Todo todos={[...todos]} />
+      <Todo />
     </div>
   );
 }
--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firestore from "../firestore";
 
 export default function Todo(props) {
+  const [todos, setTodo] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    firestore.collection("todos").onSnapshot((snapshot) => {
+      let changes = snapshot.docChanges();
+      changes.forEach((change) => {
+        if (change.type === "added")
+          setTodo((todos) => [...todos, change.doc.data()]);
+      });
+    });
+  }, []);
+
+  function handleChange(event) {
+    setNewTodo(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    firestore.collection("todos").add({ name: newTodo });
+    setNewTodo("");
+  }
+
+  console.log(todos);
   return (
     <div>
-      {props.todos.map((todo) => (
-        <h1>{todo.name}</h1>
-      ))}
+      {todos.map((todo) => {
+        return (
+          <div>
+            <h1>{todo.name}</h1>
+          </div>
+        );
+      })}
+      <input
+        type="text"
+        placeholder="add todo"
+        value={newTodo}
+        onChange={handleChange}
+      />
+      <button type="button" onClick={handleSubmit}>
+        Add
+      </button>
     </div>
   );
 }
